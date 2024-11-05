e4ec68ed6230c778d14ab2c0bcd1b55f19f0aa3a
Heeseo Kim <chungchae0@gmail.com>
2024-11-05 13:49:10 +0900

    feat: Sidebanner 애니메이션 추가

--- src/components/Sidebanner.tsx
+++ src/components/Sidebanner.tsx
@@ -1,170 +1,130 @@
-import { useRecoilState } from "recoil";
-import { activeButtonState } from "../recoil/atom";
-import { useNavigate, useLocation } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import styled from "styled-components";
-import banner from "../images/banner.png";
-import chat from "../images/banner/chat.png";
-import book from "../images/banner/book.png";
-import bookmark from "../images/banner/bookmark.png";
-import query from "../images/banner/query.png";
-import logout from "../images/adminBanner/logout.png";
 import API from "../api/axios";
+import banner from "../images/banner.png";
+
 const Sidebanner = () => {
   const navigate = useNavigate();
-  const location = useLocation();
   const [isClicked, setIsClicked] = useState(false);
-  const [activeBtn, setActiveBtn] = useRecoilState(activeButtonState);
+  const [isAnimating, setIsAnimating] = useState(false);
 
   const handleBannerClickOn = () => {
-    if (!isClicked) setIsClicked(true);
-  };
-  const handleBannerClickOff = () => {
-    if (isClicked) setIsClicked(false);
+    setIsClicked(true);
+    setIsAnimating(true);
   };
 
-  useEffect(() => {
-    const pathToKeyMap: { [key: string]: string | undefined } = {
-      "/mainpage": "mainpage",
-      "/mypage": "mypage",
-      "/myarticle": "myarticle",
-      "/queryBoard": "queryboard",
-    };
-    const currentKey = pathToKeyMap[location.pathname];
-    if (currentKey) {
-      setActiveBtn(currentKey);
-    }
-  }, [location.pathname]);
+  const handleBannerClickOff = () => {
+    setIsClicked(false);
+    setIsAnimating(true);
+  };
 
   const handleNavigate = (path: string, btnKey: string) => {
-    setActiveBtn(btnKey);
     navigate(path);
   };
 
-  const handleLogout = async (): Promise<void> => {
+  const handleLogout = async () => {
     try {
-      // API 호출
       const response = await API.get("/logout");
       alert("로그아웃되었습니다.");
       navigate("/");
-
-      // 응답 로깅
       console.log("Logout successful:", response.data);
     } catch (error) {
-      // 오류 처리
       console.error("Logout failed:", error);
     }
   };
+
   return (
     <div className="fixed top-0 right-0 z-50">
-      {!isClicked ? (
+      {!isClicked && !isAnimating ? (
         <img
           src={banner}
           className="w-[3vw] fixed top-[2vw] right-[2vw]"
           onClick={handleBannerClickOn}
         />
       ) : (
-        <div className="pointer w-[20vw] h-[50vw] bg-white rounded-bl-[2vw] p-[1vw] text-[1.5vw]">
-          <div
-            onClick={handleBannerClickOff}
-            className="text-right font-bold cursor-pointer"
+        <Container
+          isClicked={isClicked}
+          onTransitionEnd={() => {
+            if (!isClicked) {
+              setIsAnimating(false);
+            }
+          }}
+        >
+          <ToggleArrow onClick={handleBannerClickOff}>{">>"}</ToggleArrow>
+          <MenuItem
+            onClick={() => handleNavigate("/mainpage", "mainpage")}
           >
-            x
-          </div>
-          <div style={{ textAlign: "left", width: "100%" }}>
-            <p style={{ fontSize: "1vw" }}>Menu</p>
-          </div>
-          <BtnComponent>
-            <NavBarBtn
-              text="메인페이지"
-              icon={book}
-              isActive={activeBtn === "mainpage"}
-              onClick={() => handleNavigate("/mainpage", "mainpage")}
-            />
-            <NavBarBtn
-              text="나의책갈피"
-              icon={bookmark}
-              isActive={activeBtn === "mypage"}
-              onClick={() => handleNavigate("/mypage", "mypage")}
-            />
-            <NavBarBtn
-              text="나의 작성글"
-              icon={chat}
-              isActive={activeBtn === "myarticle"}
-              onClick={() => handleNavigate("/myarticle", "myarticle")}
-            />
-            <NavBarBtn
-              text="문의글 작성"
-              icon={query}
-              isActive={activeBtn === "queryboard"}
-              onClick={() => handleNavigate("/queryBoard", "queryboard")}
-            />
-            <NavBarBtn
-              text="로그아웃"
-              icon={logout}
-              isActive={activeBtn === "logout"}
-              onClick={() => handleLogout()}
-            />
-          </BtnComponent>
-        </div>
+            메인 페이지
+          </MenuItem>
+          <MenuItem
+            onClick={() => handleNavigate("/mypage", "mypage")}
+          >
+            나의 책갈피
+          </MenuItem>
+          <MenuItem
+            onClick={() => handleNavigate("/myarticle", "myarticle")}
+          >
+            나의 작성글
+          </MenuItem>
+          <MenuItem
+            onClick={() => handleNavigate("/queryBoard", "queryboard")}
+          >
+            문의글 작성
+          </MenuItem>
+          <MenuItem onClick={handleLogout}>로그아웃</MenuItem>
+        </Container>
       )}
     </div>
   );
 };
-interface NavBarBtnProps {
-  text: string;
-  icon: string;
-  onClick: () => void;
-  isActive: boolean;
-}
-interface NavBarBtnStyleProps {
-  isActive: boolean;
-}
-const NavBarBtn: React.FC<NavBarBtnProps> = ({
-  text,
-  icon,
-  onClick,
-  isActive,
-}) => {
-  return (
-    <NavBarBtnStyle onClick={onClick} isActive={isActive}>
-      {icon && (
-        <img
-          src={icon}
-          alt={text}
-          style={{ marginRight: "10px", width: "18%" }}
-        />
-      )}
-      {text}
-    </NavBarBtnStyle>
-  );
-};
 
-const NavBarBtnStyle = styled.button<NavBarBtnStyleProps>`
-  color: ${(props) => (props.isActive ? "white" : "black")};
-  background-color: ${(props) => (props.isActive ? "#5A4BFF" : "#FFFFFF")};
-  text-align: left;
-  font-weight: bold;
-  font-size: 1vw;
+const Container = styled.div<{ isClicked: boolean }>`
+  z-index: 50;
+  width: 18vw;
+  height: 100vh;
+    background-color: WHITE;
   display: flex;
-  align-items: center;
-  width: 15vw;
-  height: 2.3vw;
-  padding: 1.7vw;
-  border-radius: 0.5vw;
-  border-style: none;
-  margin: 1vw 0vw;
-  &:hover {
-    background-color: #5a4bff;
-    color: white;
+  flex-direction: column;
+  align-items: start;
+  padding: 15px;
+  border-top-left-radius: 15px;
+  border-bottom-left-radius: 15px;
+
+  animation: ${({ isClicked }) => (isClicked ? "slideIn 0.4s ease-in-out" : "none")};
+  transform: ${({ isClicked }) => (isClicked ? "none" : "translateX(100%)")};
+  transition: transform 0.4s ease-in-out;
+
+  @keyframes slideIn {
+    from {
+      transform: translateX(100%);
+    }
+    to {
+      transform: translateX(0);
+    }
   }
 `;
 
-const BtnComponent = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+
+const ToggleArrow = styled.div`
+  padding: 10px;
+  cursor: pointer;
+  font-size: 20px;
+  &:hover {
+    color: gray;
+  }
+`;
+
+const MenuItem = styled.div<{ active?: boolean }>`
   width: 100%;
+  padding: 15px;
+  font-size: 18px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+  border-radius: 5px;
+  &:hover {
+    background-color: #e0e0e0;
+  }
 `;
 
 export default Sidebanner;
