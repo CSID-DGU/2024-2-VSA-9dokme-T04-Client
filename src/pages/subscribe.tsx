3c284f0077b7ae877cd145a3b83c451a5e38f961
seulgibaek <seulgi.baek@dgu.ac.kr>
2024-12-21 14:31:17 +0900

    [feat] 구독 플랜 안내 페이지 디자인 적용

--- src/pages/subscribe.tsx
+++ src/pages/subscribe.tsx
@@ -1,40 +1,61 @@
 import React from "react";
 
-const subscribe = () => {
+const Subscribe = () => {
   return (
-    <div className="flex flex-col justify-center items-center p-20">
-      <h1>9DOKME 서비스 SubScribe</h1>
-      <div>
-        <h3>
-          ✔️9DOKME 서비스를 구독하여 전공 교재를 PDF 서비스로 이용해보세요!
-        </h3>
-        <h3>✔️커뮤니티 서비스를 이용하여 사람들과 함께 토론해보세요!</h3>
-        <h3>
-          ✔️Premium 서비스를 구독하여, 커뮤니티 질문에 대해 신속한 AI 답변
+    <div className="flex flex-col justify-center items-center px-10 min-h-screen">
+      <h1 className="text-3xl font-bold mb-8">9DOKME 서비스 구독 플랜</h1>
+      <div className="text-lg mb-6">
+        <p>
+          ✔️ 9DOKME 서비스를 구독하여 전공 교재를 PDF 서비스로 이용해보세요!
+        </p>
+        <p>✔️ 커뮤니티 서비스를 이용하여 사람들과 함께 토론해보세요!</p>
+        <p>
+          ✔️ Premium 서비스를 구독하여, 커뮤니티 질문에 대해 신속한 AI 답변
           서비스를 받아보세요!
-        </h3>
+        </p>
       </div>
-      <div>
-        <div className="flex flex-row space-between">
-          <div>Standard</div>
-          <div>월구독 9999원</div>
-          <div></div>
-        </div>
-        <div className="flex flex-row space-between">
-          <div>Premium</div>
-          <div>월구독 12999원</div>
-        </div>
-      </div>
-      <div className="flex flex-row gap-4">
-        <div className="text-[1.2vw] w-[20vw] py-[1.5vw] bg-blue-200 text-center rounded-lg shadow-lg">
+      <table className="table-auto border-collapse border border-gray-200 w-full max-w-4xl text-center">
+        <thead className="bg-gray-100">
+          <tr>
+            <th className="border border-gray-400 p-4">🎯 플랜</th>
+            <th className="border border-gray-400 p-4">💲 가격</th>
+            <th className="border border-gray-400 p-4">💡 제공 서비스</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="bg-gray-50">
+            <td className="border border-gray-300 p-4 font-medium">Standard</td>
+            <td className="border border-gray-300 p-4">월구독 9,999원</td>
+            <td className="border border-gray-300 p-4 text-left">
+              <ul className="list-disc list-inside">
+                <li>모든 교재 PDF 열람 가능</li>
+                <li>교재 Chapter, Page별 커뮤니티 기능 이용 가능</li>
+              </ul>
+            </td>
+          </tr>
+          <tr className="bg-gray-50">
+            <td className="border border-gray-300 p-4 font-medium">Premium</td>
+            <td className="border border-gray-300 p-4">월구독 12,999원</td>
+            <td className="border border-gray-300 p-4 text-left">
+              <ul className="list-disc list-inside">
+                <li>모든 교재 PDF 열람 가능</li>
+                <li>교재 Chapter, Page별 커뮤니티 기능 이용 가능</li>
+                <li>커뮤니티 질문에 대한 실시간 AI 답변 서비스 이용 가능</li>
+              </ul>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div className="flex gap-6 mt-20">
+        <button className="w-[28vw] h-[4vw] px-6 py-3 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 to-blue-500 to-90% text-white text-lg rounded-lg shadow-lg">
           Standard 플랜 구독하기
-        </div>
-        <div className="text-[1.2vw] w-[20vw] py-[1.5vw] bg-red-200 text-center rounded-lg shadow-lg">
+        </button>
+        <button className="w-[28vw] h-[4vw] px-6 py-3 text-white text-lg rounded-lg bg-gradient-to-r from-red-500 via-purple-500 to-pink-500">
           Premium 플랜 구독하기
-        </div>
+        </button>
       </div>
     </div>
   );
 };
 
-export default subscribe;
+export default Subscribe;
